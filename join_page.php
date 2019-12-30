3f433dc5d4eb3be29c71290112bf8f91ba919a03
OCRian <foldshub@gmail.com>
2019-12-31 01:01:30 +0530

    debug - fixed navbar vertical alignment

--- join_page.php
+++ join_page.php
@@ -9,7 +9,7 @@
     <?php include 'header.php' ?>
 <div class=yellow>
   <h1>
-    Would you like to join? Sign up here
+    Would you like to join? Sign up here.
   </h1>
   <p style="text-align:center;">
   <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfAqhq9YicXCRxqFYu640Y3ORmV73jReoo8NpzR_IBCL8gO9g/viewform?embedded=true" width="640" height="1200" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
